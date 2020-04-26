a0d4c1e5066f05236acab2a272ab3c9422304867
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2020-04-26 14:32:32 -0300

    Sort getTransactions response

--- src/utils/TransactionApi.tsx
+++ src/utils/TransactionApi.tsx
@@ -11,7 +11,20 @@ const TransactionApi = {
 
         try{
             const res = await fetch('http://localhost:3000/api/transactions', fetchInit);
-            return await res.json();
+            const transactions = await res.json();
+
+            transactions.sort((elementA, elementB) => {
+                const dateA = new Date(elementA.date);
+                const dateB = new Date(elementB.date);
+
+                if(dateA.getTime() < dateB.getTime()){
+                    return 1;
+                }
+
+                return -1;
+            });
+
+            return transactions;
         } catch (error){
             console.log(error);
             return  [];
